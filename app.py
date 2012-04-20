d6ecd448f459a6f755c77329e62e510f2efb349d
Dheeraj Sayala <dheerosaur@gmail.com>
2012-04-20 17:07:06 +0530

    Made game work with levels file

--- app.py
+++ app.py
@@ -1,19 +1,46 @@
 #!/usr/bin/env python
 import os
 import sys
+import readline
+
+from levels import levels
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books.settings")
 
-from authors.models import *
 
-while True:
-    inp = raw_input()
-    if inp == 'q':
-        break
-    try:
-        returned = eval(inp)
-    except Exception, e:
-        print e
-    else:
-        print 'Returned : %s' % returned
+def game_play():
+    from django.db.models.query import QuerySet
+    from authors.models import Book, Author, Publisher
+    print "Type 'q' and press RETURN to quit"
+
+    level = 1
+    level_changed = True
+
+    while True:
+        level_dict = levels[level]
+        if level_changed:
+            print level_dict.get('greet')
+        print level_dict.get('question')
+        inp = raw_input(">>> ")
+        if inp == 'q':
+            break
+        try:
+            qs = eval(inp)
+        except Exception, e:
+            print e
+        else:
+            if level_dict['test'](qs):
+                level_changed = True
+                print 'Good!'
+                level += 1
+            else:
+                level_changed = False
+                print 'Try again'
+
+
+if __name__ == "__main__":
+    readline.parse_and_bind('tab: complete')
+    readline.parse_and_bind('set editing-mode emacs')
+    game_play()
+
