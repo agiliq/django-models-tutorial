feb0370cd3e55021aeab268a0fcc32bc408c3a84
Dheeraj Sayala <dheerosaur@gmail.com>
2012-04-20 16:55:20 +0530

    More cleaning up of levels.py

--- levels.py
+++ levels.py
@@ -8,57 +8,55 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "books.settings")
 from authors.models import Book, Author, Publisher
 
 
-def level1(input):
+def level1(inp):
     return True
     
-def level2(input):
-    return input == Author.objects.all()
+def level2(inp):
+    return list(inp) == list(Author.objects.all())
 
-def level3(input):
-    return input == Author.objects.filter(first_name = "Stephen")
+def level3(inp):
+    return list(inp) == list(Author.objects.filter(first_name = "Stephen"))
     
 
 levels = [
+    {}, # To start from level1
     {
-        level: 1,
-        test: level1,
-        greet: """
+        'test': level1,
+        'greet': """
     Welcome to the Queryget tutorial.
     The interactive queryset tutorial.
     exit the prompt by typing "q"
     """,
-        question: """
+        'question': """
     What is your name?
     """,
-        goodbye: "",
+        'goodbye': "",
     },
     {
-        level: 2,
-        test: level2,
-        greet: """
+        'test': level2,
+        'greet': """
     The first article you should know about is .all.
     It will give you all the entries for a particular model.
     Eg to get all the Books, the command is
     Book.objects.all()
     """,
-        question: """
+        'question': """
         How can you get all the Authors?
         """,
-        goodbye: "",
+        'goodbye': "",
     },
     {
-        level: 2,
-        test: level2,
-        greet: """
+        'test': level3,
+        'greet': """
     The first article you should know about is .all.
     It will give you all the entries for a particular model.
     Eg to get all the Books, the command is
     Book.objects.all()
     """,
-        question: """
+        'question': """
     How can you get all the Authors?
     """,
-        goodbye: "",
+        'goodbye': "",
     }
     
 ]
